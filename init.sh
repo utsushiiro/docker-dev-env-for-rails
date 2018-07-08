a4ec38730f6833ca938b3f34bd4207dd3c2c6279
utsushiiro <ozeki.haru@gmail.com>
2018-07-08 23:20:01 +0900

    mvのオプションを修正

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@ docker-compose build
 
 docker-compose run --rm app bundle exec rails new . --force --database=mysql --skip-coffee --skip-turbolinks --skip-test
 
-mv --force database.yml config/database.yml
+mv -f database.yml config/database.yml
 
 docker-compose run --rm app bin/rails db:create
 
